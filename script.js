ee46b79dcef191ddb41dd731c2c7067e070ae777
Nihal00 <83398133+Nihal00@users.noreply.github.com>
2023-06-13 23:08:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+//your JS code here. If required.
+function loaded() {
+	document.body.innerText = `DOM load success`;
+}
+
+window.addEventListener('load', loaded);
+
